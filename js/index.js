463df41977a7aec031e3d9b6875c0ddb7e2596f0
Osvaldo <torquati.osvaldo@gmail.com>
2022-05-31 17:11:13 -0300

    Controle de Gastos
    Fatorando o código
    Melhorando a legibilidade do código

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function onChangePassword() {
 }
 
 function isEmailValid() {
-  const email = document.getElementById("email").value;
+  const email = form.email().value;
   if (!email) {
     return false;
   }
@@ -17,47 +17,42 @@ function isEmailValid() {
 }
 
 function isPassWordValid() {
-  const password = document.getElementById('password').value;
+  const password = form.password().value;
   if (!password) {
     return false;
   }
   return true;
-}
-function validateEmail(email){
-  return /\S+@\S+\.\S+/.test(email);
 }
 
 function toggleButtonsDisable(){
   // pegar o valor do campo e-mail e se o email não é vazio e se é um e-mail válido
   const emailValid = isEmailValid();
   // se verdadeiro, habilitar o botão recuperar senha
-  document.getElementById('recover-password-button').disabled = !emailValid;
+  form.recoverPassword().disabled = !emailValid;
 
   const passwordValid = isPassWordValid();
-  document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+  form.loginButton().disabled = !emailValid || !passwordValid;
   // se feito, então desabilitar o botão de recuperar senha
 }
 
 function toggleEmailErrors(){
-  const email = document.getElementById('email').value;
-  if (!email) {
-    // Mostrar mensagem de erro
-    document.getElementById('email-required-error').style.display = "block";
-  } else {
-    document.getElementById('email-required-error').style.display = "none";
-  }
-  if (validateEmail(email)) {
-    document.getElementById('email-invalid-error').style.display = "none";
-  } else {
-    document.getElementById('email-invalid-error').style.display = "block";
-  }
+  const email = form.email().value;
+  form.emailRequiredError().style.display = email ? "none" : "block";
+ 
+  form.emailInvalidError().style.display = validateEmail(email) ? "none" :"block";
 }
 
 function togglePasswordErrors(){
-  const password = document.getElementById('password').value;
-  if (!password) {
-    document.getElementById('password-required-error').style.display = "block";
-  } else {
-    document.getElementById('password-required-error').style.display = "none";
-  }
+  const password = form.password().value;
+  form.passwordRequiredError().style.display = password ? "none" : "block";
+ 
+}
+const form = {
+  email: () => document.getElementById('email'),
+  emailInvalidError: () => document.getElementById('email-invalid-error'),
+  emailRequiredError: () => document.getElementById('email-required-error'),
+  loginButton: () => document.getElementById('login-button'),
+  password: () => document.getElementById('password'),
+  passwordRequiredError: () => document.getElementById('password-required-error'),
+  recoverPassword: () =>document.getElementById('recover-password-button')
 }
